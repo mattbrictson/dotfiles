1b1b139d12c58ae694c477c6d9afdb1b70920fb7
Matt Brictson <matt@mattbrictson.com>
2016-03-05 17:04:38 -0800

    Improved history defaults from bash-sensible
    Courtesy of https://github.com/mrzool/bash-sensible

--- bashrc.d/history.bash
+++ bashrc.d/history.bash
@@ -1,4 +1,7 @@
-export HISTSIZE=10000
+# Record each line as it gets issued
+PROMPT_COMMAND='history -a'
+HISTSIZE=500000
+HISTFILESIZE=100000
 export HISTIGNORE="&:ls:l:la:ll:exit"
 bind '"\e[A"':history-search-backward # up arrow
 bind '"\e[B"':history-search-forward  # down arrow
