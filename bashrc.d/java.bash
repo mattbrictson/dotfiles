0bd8bbe3367e5e85825e182def93a0b6461be85b
Matt Brictson <matt@mattbrictson.com>
2016-10-19 09:06:36 -0700

    Add a ./gradlew alias

--- bashrc.d/java.bash
+++ bashrc.d/java.bash
@@ -2,3 +2,5 @@ if [ -d /System/Library/Frameworks/JavaVM.framework/Versions/1.6 ]; then
   export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
   export MAVEN_OPTS="-Xmx512m -XX:+HeapDumpOnOutOfMemoryError"
 fi
+
+alias gw='./gradlew --daemon'
