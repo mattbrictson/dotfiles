960b8abb6ec874d438c212fa92f18d6cdc48c6d3
Matt Brictson <matt@mattbrictson.com>
2017-04-12 08:39:08 -0700

    More reliably detect Mac UI

--- bashrc.d/env.bash
+++ bashrc.d/env.bash
@@ -3,7 +3,7 @@ export LSCOLORS=Dxxxxxxxxxxxxxxxxxxxxx
 export PAGER=/usr/bin/less
 export LESS=-RFX
 
-if [ "$TERM_PROGRAM" == "Apple_Terminal" ]; then
+if [ $Apple_PubSub_Socket_Render ]; then
   export EDITOR="subl -w"
   export BUNDLER_EDITOR=subl
   export GEM_EDITOR=subl
