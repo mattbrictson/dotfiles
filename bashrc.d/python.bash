7be9d55536545e5da5c361461b75de96371e47cf
Matt Brictson <matt@mattbrictson.com>
2017-07-20 09:05:43 -0700

    Fix virtualenv for latest brew python

--- bashrc.d/python.bash
+++ bashrc.d/python.bash
@@ -12,7 +12,7 @@ then
     export VIRTUALENV_DISTRIBUTE=true
     export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
     export PIP_REQUIRE_VIRTUALENV=true
-    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
 
     wrapper_sh=/usr/local/bin/virtualenvwrapper.sh
     [[ -f $wrapper_sh ]] && . $wrapper_sh
