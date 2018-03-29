608df5f320102fc1c26fea917eca9f7de0889133
Matt Brictson <matt@mattbrictson.com>
2018-03-28 17:44:54 -0700

    Add heroku auto-completions

--- bashrc.d/completion.bash
+++ bashrc.d/completion.bash
@@ -3,4 +3,10 @@ then
   if [ -f /usr/local/etc/bash_completion ]; then
     . /usr/local/etc/bash_completion
   fi
+  # Follow these instructions to enable Heroku CLI Autocomplete
+  # https://devcenter.heroku.com/articles/heroku-cli-autocomplete
+  CLI_ENGINE_AC_BASH_SETUP_PATH=~/Library/Caches/heroku/completions/bash_setup
+  if [ -f $CLI_ENGINE_AC_BASH_SETUP_PATH ]; then
+    . $CLI_ENGINE_AC_BASH_SETUP_PATH
+  fi
 fi
