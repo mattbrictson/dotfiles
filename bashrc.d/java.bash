c8dbee39f5260c9b06711ffcad433de3584dc683
Bo Martin <martinbo66@gmail.com>
2018-08-06 10:54:33 -0400

    Username may be different but old way doesn't work for sure.

--- bashrc.d/java.bash
+++ bashrc.d/java.bash
@@ -4,5 +4,5 @@ if [ -d /System/Library/Frameworks/JavaVM.framework/Versions/1.6 ]; then
 fi
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="~/.sdkman"
-[ -f ~/.sdkman/bin/sdkman-init.sh ] && source ~/.sdkman/bin/sdkman-init.sh
+export SDKMAN_DIR="/Users/bmartin/.sdkman"
+[ -f /Users/bmartin/.sdkman/bin/sdkman-init.sh ] && source /Users/bmartin/.sdkman/bin/sdkman-init.sh
