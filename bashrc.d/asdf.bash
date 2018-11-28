7e1c16bdc74976a51e0288e452d0f2b5fd1d8e02
Matt Brictson <matt@mattbrictson.com>
2018-11-28 14:37:45 -0800

    Manage java etc. through asdf

--- bashrc.d/asdf.bash
+++ bashrc.d/asdf.bash
@@ -1,6 +1,11 @@
 if [ -f /usr/local/opt/asdf/asdf.sh ]; then
   source /usr/local/opt/asdf/asdf.sh
 
+  [ -d $HOME/.asdf/plugins/gradle ] || asdf plugin-add gradle
+  [ -d $HOME/.asdf/plugins/java ] || asdf plugin-add java
+  [ -d $HOME/.asdf/plugins/maven ] || asdf plugin-add maven
   [ -d $HOME/.asdf/plugins/nodejs ] || asdf plugin-add nodejs
   [ -d $HOME/.asdf/plugins/ruby ] || asdf plugin-add ruby
+
+  source ~/.asdf/plugins/java/bin/asdf-java-wrapper
 fi
