7eadc01a5d15bdaf25853044d2f79c09db29e6b6
Matt Brictson <matt@mattbrictson.com>
2018-12-13 10:36:15 +0000

    Use fork to work around asdf-maven bug
    See https://github.com/skotchpine/asdf-maven/issues/15

--- bashrc.d/asdf.bash
+++ bashrc.d/asdf.bash
@@ -3,7 +3,7 @@ if [ -f /usr/local/opt/asdf/asdf.sh ]; then
 
   [ -d $HOME/.asdf/plugins/gradle ] || asdf plugin-add gradle
   [ -d $HOME/.asdf/plugins/java ] || asdf plugin-add java
-  [ -d $HOME/.asdf/plugins/maven ] || asdf plugin-add maven
+  [ -d $HOME/.asdf/plugins/maven ] || asdf plugin-add maven https://github.com/mattbrictson/asdf-maven
   [ -d $HOME/.asdf/plugins/nodejs ] || asdf plugin-add nodejs
   [ -d $HOME/.asdf/plugins/ruby ] || asdf plugin-add ruby
 
