9988f0b10b55e14f5f882df2d1388b0e44ceb3d9
Matt Brictson <matt@mattbrictson.com>
2019-04-23 23:24:31 -0700

    Switch from nvm to nodenv

--- bashrc.d/nvm.bash
+++
@@ -1,3 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
