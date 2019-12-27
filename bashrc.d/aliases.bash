c65222bb2969089146899d803732b9e82e8e42fd
Matt Brictson <matt@mattbrictson.com>
2019-12-26 21:24:17 -0600

    Add a script to open CircleCI for current git repo

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -3,7 +3,6 @@ alias bbc='bucket compare'
 alias bup='bundleup'
 alias cat='bat'
 # Change to the root level directory the current git repository
-alias cci=circleci-cli
 alias cdg='cd $(git rev-parse --show-toplevel || pwd)'
 alias diff=colordiff
 alias dbundle="$HOME/Code/bundler/bin/bundle"
