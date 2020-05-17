283639279126e5be35a1281bdc0acd3ffb44c94c
Matt Brictson <matt@mattbrictson.com>
2020-05-16 17:56:11 -0700

    Add "s" alias; retire aliases I don't use anymore

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -1,15 +1,11 @@
 alias b='bundle exec'
-alias bbc='bucket compare'
 alias bup='bundleup'
 alias cat='bat'
 # Change to the root level directory the current git repository
 alias cdg='cd $(git rev-parse --show-toplevel || pwd)'
-alias dbundle="$HOME/Code/bundler/bin/bundle"
 alias diff=colordiff
 alias gb='hub browse'
-alias gco='git co'
 alias get='git'
-alias gs='git st'
 alias hl='heroku local $(test -f Procfile.dev && echo "-f Procfile.dev")'
 alias httpserve='ruby -run -e httpd -- --port=8888 $1'
 alias l='exa'
@@ -19,8 +15,7 @@ alias ls='exa'
 alias ls='ls -hFG'
 alias n='npx --no-install'
 alias ras='bin/rails server -b 0.0.0.0 -p 3000'
-alias record="npx terminalizer --config $(readlink $HOME/.terminalizer/config.yml) record"
-alias rgi="rg $1 -g '!*.lock'"
+alias s="git sw"
 alias secret="ruby -rsecurerandom -e 'puts SecureRandom.hex(64)'"
 alias top='top -s 5 -o cpu -stats pid,user,command,cpu,rsize,vsize,threads,state'
 alias uuid="ruby -rsecurerandom -e 'puts SecureRandom.uuid'"
