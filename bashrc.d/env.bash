e788b234bde9d8545ef39997d9642f24de39c885
Matt Brictson <matt@mattbrictson.com>
2021-08-06 15:05:39 -0700

    Configure rg to search hidden files by default

--- bashrc.d/env.bash
+++ bashrc.d/env.bash
@@ -6,6 +6,7 @@ export GIT_DUET_CO_AUTHORED_BY=1
 export GIT_DUET_SECONDS_AGO_STALE=30
 export HOMEBREW_INSTALL_BADGE=🍵
 export PATH=/usr/local/opt/grep/libexec/gnubin:/usr/local/sbin:$PATH
+export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
 
 if [ $TERM_PROGRAM == 'Apple_Terminal' ]; then
   export EDITOR="subl -w"
