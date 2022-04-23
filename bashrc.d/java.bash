2592117372819956f6dda2932d3944c284a8716b
Bo Martin <martinbo66@gmail.com>
2022-04-23 19:39:59 -0400

    genericize java bash config

--- bashrc.d/java.bash
+++ bashrc.d/java.bash
@@ -1,10 +1,5 @@
-if [ -d /Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home ]; then
-  export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-  export MAVEN_OPTS="-Xmx512m -XX:+HeapDumpOnOutOfMemoryError"
-fi
-
 export JAVA_OPTS="-Xms256M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError"
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/bmartin/.sdkman"
-[ -f /Users/bmartin/.sdkman/bin/sdkman-init.sh ] && source /Users/bmartin/.sdkman/bin/sdkman-init.sh
+export SDKMAN_DIR="$HOME/.sdkman"
+[ -f $HOME/.sdkman/bin/sdkman-init.sh ] && source $HOME/.sdkman/bin/sdkman-init.sh
