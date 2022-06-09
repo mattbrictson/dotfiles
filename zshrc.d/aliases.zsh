b7c6f011cf0ff0d72d9e095e748ef79355ca59d7
Bo Martin <bmartin@lirio.co>
2022-06-09 06:54:21 -0400

    Handy Kubectl aliases

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -36,6 +36,26 @@ alias usage='df -h'
 alias space='du -Lsh ./*'
 alias wt='title ${PWD##*/}'
 
+# Kubernetes CLI specific stuff
+alias pods='kubectl get pods -o wide'
+alias k='kubectl'
+alias kg='kubectl get'
+alias kgpod='kubectl get pod'
+alias kgall='kubectl get --all-namespaces all'
+alias kdp='kubectl describe pod'
+# kubectl apply
+alias kap='kubectl apply'
+# kubectl delete
+alias krm='kubectl delete'
+alias krmf='kubectl delete -f'
+# kubectl services
+alias kgsvc='kubectl get service'
+# kubectl deployments
+alias kgdep='kubectl get deployments'
+# kubectl misc
+alias kl='kubectl logs'
+alias kei='kubectl exec -it'
+
 # Function for naming terminal tabs
 function title {
     echo -ne "\e]1;"$*"\a"
