407b47af0eeae91fd831a16d17920b38319c977f
Bo Martin <bmartin@lirio.co>
2022-08-10 11:15:45 -0400

    Fix completions

--- zshrc.d/completions.zsh
+++ zshrc.d/completions.zsh
@@ -1,2 +1,8 @@
 fpath=(~/.zshrc.d $fpath)
-eval "$(op completion zsh)"; compdef _op op
+# eval "$(op completion zsh)"; compdef _op op
+if type brew &>/dev/null; then
+  export fpath=(${HOMEBREW_PREFIX}/share/zsh-completions $fpath)
+
+  autoload -Uz compinit
+  compinit
+fi
