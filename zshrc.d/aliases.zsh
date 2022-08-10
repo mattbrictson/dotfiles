a72cc13160710cddd7d4f13aca90d19259306134
Bo Martin <bmartin@lirio.co>
2022-08-10 11:14:54 -0400

    Brew install and update without doing the auto update

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -1,3 +1,5 @@
+alias bi='HOMEBREW_NO_AUTO_UPDATE=1 brew install'
+alias bu='HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade'
 alias br='for k in `git branch | sed s/^..//`; do echo -e `git log -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k --`\\t"$k";done | sort'
 alias c='clear'
 alias cat='ccat'
