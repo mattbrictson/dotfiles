aefc6244637622f458d17265af6b7b5af481523b
Matt Brictson <matt@mattbrictson.com>
2022-12-28 18:48:27 -0800

    Install pyenv for python projects

--- bashrc.d/python.bash
+++ bashrc.d/python.bash
@@ -1,24 +1,2 @@
-# You will need to:
-#   brew install python2
-#   syspip install --upgrade virtualenv
-#   syspip install --upgrade virtualenvwrapper
-#   mkdir ~/.virtualenvs
-
-if [[ $- == *i* ]]
-then
-  if [ -d ~/.virtualenvs ]; then
-    export WORKON_HOME=$HOME/.virtualenvs
-    export PIP_VIRTUALENV_BASE=$WORKON_HOME
-    export VIRTUALENV_DISTRIBUTE=true
-    export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-    export PIP_REQUIRE_VIRTUALENV=true
-    export VIRTUALENVWRAPPER_PYTHON=$BREW_PREFIX/bin/python
-
-    wrapper_sh=$BREW_PREFIX/bin/virtualenvwrapper.sh
-    [[ -f $wrapper_sh ]] && . $wrapper_sh
-
-    syspip(){
-      PIP_REQUIRE_VIRTUALENV="" pip "$@"
-    }
-  fi
-fi
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
