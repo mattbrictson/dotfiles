d50759c3715a23ad8df5fc62a8bae75daf7f1c5e
Matt Brictson <matt@mattbrictson.com>
2023-08-04 20:13:52 -0700

    Move keybindings into inputrc and improve config

--- bashrc.d/history.bash
+++ bashrc.d/history.bash
@@ -3,5 +3,3 @@ PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ;} history -a"
 HISTSIZE=500000
 HISTFILESIZE=100000
 export HISTIGNORE="&:ls:l:la:ll:exit"
-bind '"\e[A"':history-search-backward # up arrow
-bind '"\e[B"':history-search-forward  # down arrow
