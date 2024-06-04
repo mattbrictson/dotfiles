c67039c60a1b1fd219b5f1fa7d27b59a8e161d08
Matt Brictson <matt@mattbrictson.com>
2024-06-04 16:16:58 -0700

    Install mcfly for smarter command history

--- bashrc.d/history.bash
+++ bashrc.d/history.bash
@@ -3,3 +3,7 @@ PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ;} history -a"
 HISTSIZE=500000
 HISTFILESIZE=100000
 export HISTIGNORE="&:ls:l:la:ll:exit"
+
+# Rebind ctrl-r with to show smart history search
+export MCFLY_RESULTS=16
+eval "$(mcfly init bash)"
